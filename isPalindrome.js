b751d3e91c27253207f7f0dc67060edaf4e9bbdd
Olimpiada Lysenko <olimpiadalys@gmail.com>
2016-04-28 10:34:29 +0300

    add recursion
    add recursion

--- isPalindrome.js
+++ isPalindrome.js
@@ -13,3 +13,15 @@ function isPalindrome(string)
 
 	  return false
 }
+/////////////////
+function isPalindrome(string) 
+{
+  
+	if(string.length <= 1)
+	  return true
+
+	if(string.charAt(0) != string.charAt(string.length - 1))
+	  return false
+
+	return isPalindrome(string.substr(1, string.length - 2))
+}
